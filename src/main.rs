7593b097f01668efd8d5c0c60799d694db7f96e6
a7raser <a7raser@gmail.com>
2021-06-14 19:54:28 +0100

    some, not a lot

--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,34 @@ fn repo_status(repo: &Repository, detail: bool) -> Option<String> {
     if !detail {
         if let Some((ic, wtc, conflict, untracked)) = count_statuses(repo) {
             if ic != 0 || wtc != 0 || conflict != 0 || untracked !=0 {
-                output.push(format!("%F{{{}}}⬣%f", RED));
+                output.push(format!("%F{{{}}} ✱%f", RED));
             }
         }
     } else {
+        println!("test");
         if let Some((ahead, behind)) = get_ahead_behind(repo) {
             if ahead > 0 {
-                output.push(format!("%F{{{}}}↑{}%f", YELLOW, ahead));
+                output.push(format!("%F{{{}}} ↑{}%f", YELLOW, ahead));
             }
             if behind > 0 {
-                output.push(format!("%F{{{}}}↓{}%F", ORANGE, behind));
+                output.push(format!("%F{{{}}} ↓{}%F", ORANGE, behind));
             }
         }
         if let Some((ic, wtc, conflict, untracked)) = count_statuses(repo) {
             if ic == 0 && wtc == 0 && conflict == 0 && untracked == 0 {
-                output.push(format!("%F{{{}}}✔✓%f", GREEN));
+                output.push(format!("%F{{{}}} ✔%f", GREEN));
             } else {
                 if ic > 0 {
-                    output.push(format!("%F{{{}}}⬣{}", YELLOW, ic));
+                    output.push(format!("%F{{{}}} ⊘{}", YELLOW, ic));
                 }
                 if conflict > 0 {
-                    output.push(format!("%F{{{}}}‼{}", RED, conflict));
+                    output.push(format!("%F{{{}}} ✘{}", RED, conflict));
                 }
                 if wtc > 0 {
-                    output.push(format!("%F{{{}}}⬢{}", YELLOW, wtc));
+                    output.push(format!("%F{{{}}} ⁑{}", YELLOW, wtc));
                 }
                 if untracked > 0 {
-                    output.push(format!("%F{{{}}}…%f", ORANGE));
+                    output.push(format!("%F{{{}}} …%f", ORANGE));
                 }
             }
         }
